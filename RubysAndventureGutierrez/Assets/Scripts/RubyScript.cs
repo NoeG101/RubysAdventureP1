e5c75ccba4e942dede25c0daa06b661dbd974a41
NoeG101 <116104392+NoeG101@users.noreply.github.com>
2022-11-16 09:57:48 -0800

    we created an enviornment and decorations
    enviornment and decoration

--- RubysAndventureGutierrez/Assets/Scripts/RubyScript.cs
+++ RubysAndventureGutierrez/Assets/Scripts/RubyScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RubyScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,8 @@ public class NewBehaviourScript : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
