2343bc71183bc56e70d48df438247a6540bf432d
NoeG101 <116104392+NoeG101@users.noreply.github.com>
2022-12-09 09:57:53 -0800

    Ruby's Anventure
    finished ruby's adventure

---
+++ RubysAndventureGutierrez/Assets/Scripts/ExerciseExposeAnotherVariable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExerciseExposeAnotherVariable : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
